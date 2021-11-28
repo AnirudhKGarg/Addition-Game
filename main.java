41cbf27cd908915c13663fb2208e7bb01364cad0
AnirudhKGarg <91921469+AnirudhKGarg@users.noreply.github.com>
2021-11-28 00:03:21 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+//Import my libraries
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+import java.util.Random;
+
+public static void main(String[] args) {
+new math_tool();
+} 
